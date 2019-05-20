bb266ed4dee43c85d06617928c711bba04959311
raze92 <michael.radzieda@outlook.com>
2019-05-20 18:29:44 +0800

    Add get_tags_from_tag() function.

--- hashcrawler.py
+++ hashcrawler.py
@@ -51,20 +51,59 @@ def get_tags_from_post(post_url: str):
 
 
 def get_tags_from_user(user: str, num_of_posts: int = 9):
-    """Reads all used hashtags from user of latest posts.
+    """Reads all used hashtags from latest posts of user.
     
     Arguments:
         user {str} -- Username.
     
     Keyword Arguments:
-        num_of_posts {int} -- Number of latest posts being used. (default: {9})
+        num_of_posts {int} -- Number of latest posts will be used. (default: {9})
     
     Returns:
         [str] -- List of used hashtags.
     """
     
+    # Read shared Data
     data = get_shared_data(USER_URL + user)
     posts = data['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges']
+
+    # Read tags of recent posts
+    tags = []
+    for post in posts[:num_of_posts]:
+        post_tags = get_tags_from_post(POST_URL + post['node']['shortcode'])
+        tags.extend(x for x in post_tags if x not in tags)
+
+    return tags
+
+def get_tags_from_tag(tag: str, top_posts: bool = False, num_of_posts: int = 9):
+    """Reads all used hashtags from latest or top posts of tag page.
+    
+    Arguments:
+        tag {str} -- Tag name.
+    
+    Keyword Arguments:
+        top_posts {bool} -- If True uses the top posts instead of recent posts. (default: {False})
+        num_of_posts {int} -- Number of posts will be used. (default: {9})
+    
+    Returns:
+        [str] -- List of used hashtags.
+    """
+    
+    # Read shared Data
+    data = get_shared_data(TAG_URL + tag)
+    
+    # Get Posts
+    posts = None
+    if top_posts: # Get top posts
+        posts = data['entry_data']['TagPage'][0]['graphql']['hashtag']['edge_hashtag_to_top_posts']['edges']
+
+        if num_of_posts > 9:
+            print('Warning: On top posts maximum 9 posts are possible!')
+            num_of_posts = 9
+    else: # Get recent posts
+        posts = data['entry_data']['TagPage'][0]['graphql']['hashtag']['edge_hashtag_to_media']['edges']
+
+    # Read tags of posts
     tags = []
     for post in posts[:num_of_posts]:
         post_tags = get_tags_from_post(POST_URL + post['node']['shortcode'])
@@ -85,6 +124,7 @@ def get_count_of_posts(tag: str):
 
     # Read shared Data
     data = get_shared_data(TAG_URL + tag)
+
     return int(data['entry_data']['TagPage'][0]['graphql']['hashtag']['edge_hashtag_to_media']['count'])
 
 
